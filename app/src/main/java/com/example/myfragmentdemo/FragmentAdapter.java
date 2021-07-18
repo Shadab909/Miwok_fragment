a451beee4b37e2bbaa647bff774f47db65063fe7
Shadab909 <sadabkhan90063@gmail.com>
2021-07-18 09:13:30 +0530

    Miwok Fragment

---
+++ app/src/main/java/com/example/myfragmentdemo/FragmentAdapter.java
@@ -0,0 +1,35 @@
+package com.example.myfragmentdemo;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class FragmentAdapter extends FragmentStateAdapter {
+    public FragmentAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+    }
+
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+
+        switch (position)
+        {
+            case 1:
+                return new FamilyFragment();
+            case 2:
+                return new ColorsFragment();
+            case 3:
+                return new PhrasesFragment();
+        }
+        return new NumbersFragment();
+    }
+
+    @Override
+    public int getItemCount() {
+        return 4;
+    }
+}
---
+++ app/src/main/java/com/example/myfragmentdemo/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.myfragmentdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager2 viewPager2;
+    FragmentAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        viewPager2 = (ViewPager2) findViewById(R.id.view_pager);
+
+        FragmentManager fm = getSupportFragmentManager();
+        adapter = new FragmentAdapter(fm,getLifecycle());
+        viewPager2.setAdapter(adapter);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Numbers"));
+        tabLayout.addTab(tabLayout.newTab().setText("Family"));
+        tabLayout.addTab(tabLayout.newTab().setText("Colors"));
+        tabLayout.addTab(tabLayout.newTab().setText("Phrases"));
+
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
+    }
+}
